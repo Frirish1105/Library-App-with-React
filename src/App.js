54757ffb82174fefb01d926963cdc5656b75ddab
Ryan <rynicolay11@gmail.com>
2022-11-15 18:39:26 -0600

    edit homepage path

--- src/App.js
+++ src/App.js
@@ -49,7 +49,7 @@ function App() {
   <Router>
     <div className="App">
       <Nav numberOfItems={numberOfItems()} />
-      <Route path="/" exact component={Home} />
+      <Route path="/Library-React-App" exact component={Home} />
       <Route path="/books" exact render={() => <Books books={books } />} />
       <Route path="/books/:id" render={() => <BookInfo books={books} addToCart={addToCart} cart={cart} />} />
       <Route path="/cart" render={() => <Cart books={books} cart={cart} changeQuantity={changeQuantity} removeItem={removeItem}/>} />
