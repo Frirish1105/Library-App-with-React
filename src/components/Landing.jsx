10166ee2b2ac75d02030e5d4af1537db280cbede
Ryan <rynicolay11@gmail.com>
2022-11-15 21:27:51 -0600

    Fixed button problems on landing page

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
 import UndrawBooks from '../assets/Undraw_Books.svg';
 
 const Landing = () => {
@@ -10,8 +9,8 @@ const Landing = () => {
                 <div className="header__description">
                 <h1>America's most awarded online library platform</h1>
                 <h2>Find your dream book with <span className="purple">Library</span></h2>
-                <a href="#features">
-            <button className="btn">Browse books</button>
+          <a href="/">
+            <button className="btn" onClick={() => alert("Anchor tags are incompatible with Github Pages while using Hash Router")}>Browse books</button>
           </a>
             </div>
             <figure className="header__img--wrapper">
